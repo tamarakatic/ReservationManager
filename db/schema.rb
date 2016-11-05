61bbb6f86c84f9df73fb2279549f7a8b0b2b8f8c
nemanja-m <nemanja_milicevic@hotmail.com>
2016-11-05 21:27:31 +0100

    Validate Drink model and write tests

--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20161105175345) do
   create_table "drinks", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.decimal  "price"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.decimal  "price",         precision: 8, scale: 2
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.integer  "restaurant_id"
     t.index ["restaurant_id"], name: "index_drinks_on_restaurant_id", using: :btree
   end
@@ -49,6 +49,28 @@ ActiveRecord::Schema.define(version: 20161105175345) do
     t.datetime "updated_at",    null: false
     t.integer  "restaurant_id"
     t.index ["restaurant_id"], name: "index_seats_on_restaurant_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "drinks", "restaurants"
--- test/models/drink_test.rb
+++ test/models/drink_test.rb
@@ -1,7 +1,69 @@
 require 'test_helper'
 
 class DrinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "the name should be unique" do
+    drink = Drink.new(description:  "Test",
+                      price:        42,
+                      restaurant:   restaurants(:one))
+
+    drink.name = "MyString"
+
+    assert drink.invalid?
+    assert_equal ["has already been taken"], drink.errors[:name]
+
+    drink.name = "Test"
+    assert drink.valid?
+  end
+
+  test "the attributes should be present" do
+    drink = Drink.new
+
+    assert drink.invalid?
+    assert_includes drink.errors[:name], "can't be blank"
+    assert_includes drink.errors[:description], "can't be blank"
+    assert_includes drink.errors[:price], "can't be blank"
+
+    drink.name        = "Test"
+    drink.description = "Test"
+    drink.price       = 42
+    drink.restaurant  = restaurants(:one)
+
+    assert drink.valid?
+  end
+
+  test "the price should be positive number" do
+    drink = Drink.new(name:         "Test",
+                      description:  "Test",
+                      restaurant:   restaurants(:one))
+
+    drink.price = -1
+
+    assert drink.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], drink.errors[:price]
+
+    drink.price = 5
+    assert drink.valid?
+  end
+
+  test "the length of name and description must be in given interval" do
+    drink = Drink.new(name:         "Test" * 8,
+                      description:  "Test" * 80,
+                      price:        42,
+                      restaurant:   restaurants(:one))
+
+    assert drink.invalid?
+    assert_equal ["is too long (maximum is 30 characters)"], drink.errors[:name]
+    assert_equal ["is too long (maximum is 300 characters)"], drink.errors[:description]
+
+    drink.name = "A"
+    assert drink.invalid?
+    assert_equal ["is too short (minimum is 2 characters)"], drink.errors[:name]
+
+    drink.name        = "Test"
+    drink.description = "Test"
+
+    assert drink.valid?
+  end
+
 end
