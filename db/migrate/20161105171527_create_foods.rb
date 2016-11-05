794ef3de45a979b20665c37c628c8665a1a56d5d
tamarakatic <tamarakatic@gmail.com>
2016-11-05 20:56:13 +0000

    Add validation and unit test for food and seat

--- db/migrate/20161105171527_create_foods.rb
+++ db/migrate/20161105171527_create_foods.rb
@@ -3,7 +3,7 @@ class CreateFoods < ActiveRecord::Migration[5.0]
     create_table :foods do |t|
       t.string :name
       t.text :description
-      t.decimal :price
+      t.decimal :price, precision: 8, scale: 2
 
       t.timestamps
     end
