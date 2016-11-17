244f0916fbab0db742553ac70e41173b32d6c168
tamarakatic <tamarakatic@gmail.com>
2016-11-17 23:19:58 +0100

    Validate models

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,7 +3,7 @@ class Employee < ApplicationRecord
   enum clothing_size: ['XS', 'S', 'M', 'L', 'XL', 'XXL']
 
   belongs_to :manager
-
+    
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,6 +1,14 @@
 class Provider < ApplicationRecord
   belongs_to :manager
-  
+
+  validates :firstname, :presence => true,
+                        :length => { :in => 2..30 },
+                        :allow_blank => false
+
+  validates :lastname, :presence => true,
+                        :length => { :in => 2..30 },
+                        :allow_blank => false
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 end
