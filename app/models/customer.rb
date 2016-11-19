48f8b1166cf3c69d7c702719e49ff498a819a0d2
nemanja-m <nemanja_milicevic@hotmail.com>
2016-11-19 15:48:03 +0100

    Validate Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,12 @@
 class Customer < ApplicationRecord
+  validates :firstname, :presence => true,
+                        :allow_blank => false,
+                        :length => { :in => 2..30 }
+
+  validates :lastname, :presence => true,
+                       :allow_blank => false,
+                       :length => { :in => 2..30 }
+
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 end
