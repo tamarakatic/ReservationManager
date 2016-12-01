3f4ba4fd4ce9db95f0353d7e509ee909f09d9363
nemanja-m <nemanja_milicevic@hotmail.com>
2016-12-01 08:03:22 +0100

    Sign in users in controller tests

--- spec/controllers/home_page/manager_controller_spec.rb
+++ spec/controllers/home_page/manager_controller_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe HomePage::ManagerHomeController, type: :controller do
 
   describe "GET #index" do
+    login_user(:manager)
+
     it "returns http success" do
+      p subject.current_manager
       get :index
       expect(response).to have_http_status(:success)
     end
