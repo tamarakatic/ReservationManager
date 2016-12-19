dae47ade11af3e2f58b80799b3c5f8d43088ee8c
Predrag <pele-94@hotmail.com>
2016-12-19 01:28:12 +0100

    Add password confirmed field to Employees

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161217144755) do
+ActiveRecord::Schema.define(version: 20161219002624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,12 +50,12 @@ ActiveRecord::Schema.define(version: 20161217144755) do
   end
 
   create_table "employees", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -64,8 +64,15 @@ ActiveRecord::Schema.define(version: 20161217144755) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string   "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer  "invitation_limit"
+    t.integer  "invited_by_id"
+    t.string   "invited_by_type"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "firstname"
     t.string   "lastname"
     t.integer  "role"
@@ -73,8 +80,10 @@ ActiveRecord::Schema.define(version: 20161217144755) do
     t.date     "birth_date"
     t.float    "shoe_size"
     t.integer  "manager_id"
+    t.boolean  "password_changed",       default: false
     t.index ["confirmation_token"], name: "index_employees_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_employees_on_email", unique: true, using: :btree
+    t.index ["invitation_token"], name: "index_employees_on_invitation_token", unique: true, using: :btree
     t.index ["manager_id"], name: "index_employees_on_manager_id", using: :btree
     t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true, using: :btree
   end
