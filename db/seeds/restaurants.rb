ca8493461e51c82955f9222e055d301a8b472bd6
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-02-25 22:28:43 +0000

    Move restaurant seeds to a separate files

---
+++ db/seeds/restaurants.rb
@@ -0,0 +1,44 @@
+Restaurant.delete_all
+
+
+pera = Manager.create!(:firstname             => "Pera",
+                       :lastname              => "Peric",
+                       :email                 => "peric@gmail.com",
+                       :password              => "peraperic",
+                       :password_confirmation => "peraperic",
+                       :confirmed_at          => Time.now)
+
+pera.create_restaurant!(:title       => "Black Swan",
+                        :description => "Our impressive menu of nationally renowned dry aged " +
+                                         "steaks and the freshest of seafood will ignite your " +
+                                         "culinary imagination as our award-winning wine list " +
+                                         "of more than 5,000 bottles awakens your inner sommelier")
+
+pera.restaurant.foods.create!(:name        => "Steak Tartare",
+                              :description => "Dry aged steak with avocado.",
+                              :price       => 45.0)
+
+pera.restaurant.drinks.create!(:name        => "Rakija",
+                               :description => "Strong alcholic beverage",
+                               :price       => 10.0)
+
+pera.employees.create!(:firstname             => "Ana",
+                       :lastname              => "Spasic",
+                       :email                 => "ana@gmail.com",
+                       :password              => "anaspasic",
+                       :password_confirmation => "anaspasic",
+                       :confirmed_at          => Time.now,
+                       :clothing_size         => "S",
+                       :birth_date            => Date.new(1994, 5, 5),
+                       :shoe_size             => 42)
+
+pera.restaurant.seats.create!(:area => "Garden I")
+pera.restaurant.seats.create!(:area => "Garden II")
+pera.restaurant.seats.create!(:area => "Garden III")
+
+pera.restaurant.providers.create!(:firstname             => "Misa",
+                                  :lastname              => "Misic",
+                                  :email                 => "misko@gmail.com",
+                                  :password              => "misamisic",
+                                  :password_confirmation => "misamisic",
+                                  :confirmed_at          => Time.now)
---
+++ db/seeds/system_managers.rb
@@ -0,0 +1,7 @@
+SystemManager.delete_all
+
+SystemManager.create!(:firstname => "Chuck",
+                      :lastname => 'Norris',
+                      :email => "admin@gmail.com",
+                      :password => "sistem",
+                      :password_confirmation => "sistem")
