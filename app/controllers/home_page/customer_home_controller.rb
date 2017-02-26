d76c2d51c755a48281e71d759b1bc6d6f6ebacb1
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-02-26 19:30:35 +0000

    Show restaurants as cards on home page

--- app/controllers/home_page/customer_home_controller.rb
+++ app/controllers/home_page/customer_home_controller.rb
@@ -1,36 +1,10 @@
 class HomePage::CustomerHomeController < ApplicationController
   before_action :authenticate_customer!
 
-  layout "home_page"
+  layout "customer_home"
 
   def index
-    @friends         = current_customer.friends
-    @friend_requests = current_customer.requested_friends
-    @users           = Customer.where("id != #{current_customer.id}")
-  end
-
-  # POST /send_friend_request
-  def send_friend_request
-    friend = Customer.find(params[:friend_id])
-    current_customer.friend_request(friend)
-  end
-
-  # POST /accept_friend_request
-  def accept_friend_request
-    friend = Customer.find(params[:friend_id])
-    current_customer.accept_request(friend)
-  end
-
-  # POST / decline_friend_request
-  def decline_friend_request
-    friend = Customer.find(params[:friend_id])
-    current_customer.decline_request(friend)
-  end
-
-  # DELETE / remove_friend
-  def remove_friend
-    friend = Customer.find(params[:friend_id])
-    current_customer.remove_friend(friend)
+    @restaurants = Restaurant.all
   end
 
 end
