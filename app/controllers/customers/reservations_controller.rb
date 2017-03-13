449cb4d512f4079f5b910648dfa30f3fbbb813e2
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-03-13 14:42:59 +0000

    Check if table is reserved from model

--- app/controllers/customers/reservations_controller.rb
+++ app/controllers/customers/reservations_controller.rb
@@ -22,22 +22,16 @@ class Customers::ReservationsController < ApplicationController
   def available_tables
     @restaurant = Restaurant.find(params[:restaurant_id])
 
-    new_reservation_start = to_date_time(params[:reservation_start])
-    new_reservation_end   = to_date_time(params[:reservation_end])
+    reservation_start = to_date_time(params[:reservation_start])
+    reservation_end   = to_date_time(params[:reservation_end])
 
     tables = { :available => [], :reserved => [] }
 
     @restaurant.tables.each do |table|
-      reserved = table.reservations.any? do |reservation|
-        interval_overlaps?(reservation.reserved_from,
-                           reservation.reserved_to,
-                           new_reservation_start,
-                           new_reservation_end)
-      end
-
       table_hash = table.as_json.merge(:area_name => table.seat.area,
                                        :area_id   => table.seat.id)
-      if reserved
+
+      if table.reserved?(reservation_start, reservation_end)
         tables[:reserved] << table_hash
       else
         tables[:available] << table_hash
@@ -125,10 +119,6 @@ class Customers::ReservationsController < ApplicationController
 
   def to_date_time(ms)
     Time.at(ms.to_f / 1000)
-  end
-
-  def interval_overlaps?(first_start, first_end, second_start, second_end)
-    (first_start - second_end) * (second_start - first_end) >= 0
   end
 
   def create_order(reservation)
