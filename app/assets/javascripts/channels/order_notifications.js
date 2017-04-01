69f5b5c800dab96071966c4703c10d9c6f89141c
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-04-02 00:59:54 +0200

    Subscribe to order notification channel only on provider's page

--- app/assets/javascripts/channels/order_notifications.js
+++ app/assets/javascripts/channels/order_notifications.js
@@ -1,10 +1,15 @@
-App.order_notifications = App.cable.subscriptions.create("OrderNotificationsChannel", {
-  received: function (notification) {
-    $("#provider-notifications").removeClass('hidden')
-    return $("#provider-notifications").html(this.renderNotification(notification));
-  },
-  renderNotification: function (data) {
-    var show_msg = "Show Orders!";
-    return "<p class='text-center'>" + data.notification + show_msg.link(".") + "</p>";
+$(function() {
+  if ($(document.body).data("page-name") === "provider_home") {
+
+    App.order_notifications = App.cable.subscriptions.create("OrderNotificationsChannel", {
+      received: function (notification) {
+        $("#provider-notifications").removeClass('hidden')
+        return $("#provider-notifications").html(this.renderNotification(notification));
+      },
+      renderNotification: function (data) {
+        var show_msg = "Show Orders!";
+        return "<p class='text-center'>" + data.notification + show_msg.link(".") + "</p>";
+      }
+    });
   }
-});
+}):
