9128297041271f129569943acf8dbc81839002bf
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-04-01 22:30:56 +0200

    Subscribe to provider notification channel only on provider's page

--- app/assets/javascripts/channels/provider_notifications.js
+++ app/assets/javascripts/channels/provider_notifications.js
@@ -1,17 +1,23 @@
-App.provider_notifications = App.cable.subscriptions.create("ProviderNotificationsChannel", {
-  received: function (notification) {
-    $("#provider-notifications").removeClass('hidden')
-    return $("#provider-notifications").html(this.renderNotification(notification));
-  },
-  renderNotification: function (data) {
-    console.log(data)
-    var user = $("#current_user").data("provider")
+$(function() {
+  if ($(document.body).data("page-name") === "provider_home") {
 
-    if (data.confirmed_provider === user) {
-      return "<p class='text-center'>" + data.confirmed_offer + data.message_confirmed + "</p>";
-    }
-    else if (data.rejected_providers.indexOf(user) != -1) {
-      return "<p class='text-center'>" + data.message_rejected + "</p>";
-    }
+    App.provider_notifications = App.cable.subscriptions.create("ProviderNotificationsChannel", {
+      received: function (notification) {
+        $("#provider-notifications").removeClass('hidden')
+        return $("#provider-notifications").html(this.renderNotification(notification));
+      },
+      renderNotification: function (data) {
+        console.log(data)
+        var user = $("#current_user").data("provider")
+
+        if (data.confirmed_provider === user) {
+          return "<p class='text-center'>" + data.confirmed_offer + data.message_confirmed + "</p>";
+        }
+        else if (data.rejected_providers.indexOf(user) != -1) {
+          return "<p class='text-center'>" + data.message_rejected + "</p>";
+        }
+      }
+    });
   }
 });
+
--- app/assets/javascripts/home_page/employee_home.coffee
+++ app/assets/javascripts/home_page/employee_home.coffee
@@ -2,14 +2,13 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
-$ ->
-  $(document).on 'ready turbolinks:load', ->
+$(document).on 'ready turbolinks:load', ->
 
-    id = $('#empId').val()
-    arrNotif = Persist.load(id,'local')
-    if arrNotif == null
-      return
+  id = $('#empId').val()
+  arrNotif = Persist.load(id,'local')
+  if arrNotif == null
+    return
 
-    if arrNotif.length > 0
-      for x of arrNotif
-        $('#notifs').append("<p>"+arrNotif[x].content+". Messsage from "+arrNotif[x].firstname+" "+arrNotif[x].lastname+"</p>")
+  if arrNotif.length > 0
+    for x of arrNotif
+      $('#notifs').append("<p>"+arrNotif[x].content+". Messsage from "+arrNotif[x].firstname+" "+arrNotif[x].lastname+"</p>")
