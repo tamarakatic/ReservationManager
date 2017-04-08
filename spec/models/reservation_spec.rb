210f76c58a8e08dbbf76ac4a94af8131f7f3487f
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-04-08 12:00:28 +0200

    Fix Reservation specs

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,39 +1,105 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Reservation, type: :model do
-  describe "Associations" do
-    it { should belong_to(:restaurant) }
-    it { should belong_to(:customer) }
-    it { should have_many(:reservation_invitations) }
-    it { should have_many(:number_of_seats) }
+  subject { FactoryGirl.create(:reservation, :reserved_from => Time.now + 1.hours,
+                                             :reserved_to   => Time.now + 2.hours) }
+
+  it { should belong_to(:restaurant) }
+  it { should belong_to(:customer) }
+  it { should have_many(:reservation_invitations).dependent(:destroy) }
+  it { should have_many(:reserved_tables).dependent(:destroy) }
+  it { should have_many(:number_of_seats).through(:reserved_tables) }
+  it { should have_many(:reservation_orders) }
+  it { should have_many(:customer_orders).through(:reservation_orders).dependent(:destroy) }
+
+  describe "attributes" do
+    it { should respond_to(:host) }
+    it { should respond_to(:invitations) }
+    it { should respond_to(:tables) }
   end
 
   describe "validations" do
-    let!(:reservation) { build(:reservation) }
 
     context "when reservation end date is after begin date" do
       it "is valid" do
-        expect(reservation).to be_valid
+        expect(subject).to be_valid
       end
     end
 
     context "when end of reservation date is lower than begin date" do
       before do
-        reservation.reserved_to = reservation.reserved_from - 5.hours
+        subject.reserved_to = subject.reserved_from - 5.hours
       end
 
       it "is invalid" do
-        expect(reservation).to_not be_valid
+        expect(subject).to_not be_valid
       end
     end
 
     context "when reservation day is in past" do
       before do
-        reservation.reserved_from = DateTime.now - 5.days
+        subject.reserved_from = DateTime.now - 5.days
       end
 
       it "is invaid" do
-        expect(reservation).to_not be_valid
+        expect(subject).to_not be_valid
+      end
+    end
+  end
+
+  describe "#acitve?" do
+    context "when reservation start date is after current time" do
+      before do
+        subject.reserved_from = Time.now + 1.hour
+      end
+
+      it "returns true" do
+        expect(subject.active?).to be_truthy
+      end
+    end
+
+    context "when reservation start date is before current time" do
+      before do
+        subject.reserved_from = Time.now - 1.hour
+      end
+
+      it "returns false" do
+        expect(subject.active?).to be_falsy
+      end
+    end
+  end
+
+  describe "#cancelable?" do
+    context "when there is more than 30 minutes from reservation start" do
+      before do
+        subject.reserved_from = Time.now + 1.hour
+      end
+
+      it "returns true" do
+        expect(subject.cancelable?).to be_truthy
+      end
+    end
+
+    context "when there is less than 30 minutes from reservation start" do
+      before do
+        subject.reserved_from = Time.now + 29.minutes
+      end
+
+      it "returns false" do
+        expect(subject.cancelable?).to be_falsy
+      end
+    end
+  end
+
+  describe "#duration_in_hours" do
+    context "given start time is 12:00 and end time is 13:00" do
+      before do
+        subject.reserved_from = DateTime.now.noon
+        subject.reserved_to   = DateTime.now.noon + 1.hour
+      end
+
+      it "returns 1 hour" do
+        expect(subject.duration_in_hours).to eql 1.0
       end
     end
   end
